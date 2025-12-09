af391c949e9a4c8acb89ac18d4d1673244135cf8
jmncamilo <jmncamilo@gmail.com>
2025-12-08 23:48:59 -0500

    feat(components): implement early return in the alert modal component

--- src/components/modals/alert/AlertModal.js
+++ src/components/modals/alert/AlertModal.js
@@ -1,4 +1,9 @@
-export function AlertModal() {
+import styles from "./AlertModal.module.css";
+
+export function AlertModal({ isOpen }) {
+
+    if (!isOpen) return null;
+
     return (
         <div className="fixed inset-0 bg-black/40 backdrop-blur-md backdrop-saturate-150 flex items-center justify-center p-0 md:p-6 lg:p-8 z-50 overflow-hidden">
             <div className="w-full max-w-full min-w-[300px] max-h-[85vh] min-h-[300px] rounded-[5rem] md:max-w-[600px] md:max-h-[80vh] md:min-h-[350px] lg:max-w-[700px] lg:max-h-[75vh] lg:min-h-[300px] bg-[#f0f0f0] md:rounded-[3.5rem] shadow-[0_4px_13px_0_rgba(0,0,0,0.75)] flex flex-col justify-center items-center overflow-hidden py-14">
