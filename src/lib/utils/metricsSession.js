90f624c339f97b4c1d5a6278185a6284e4878852
jmncamilo <jmncamilo@gmail.com>
2025-12-11 00:01:50 -0500

    feat(lib): add method to get customer environmental metrics in local storage

--- src/lib/utils/metricsSession.js
+++ src/lib/utils/metricsSession.js
@@ -1,3 +1,8 @@
 export function setMetrics(carbon, nitrogen, water) {
     localStorage.setItem('metricsSession', JSON.stringify({ carbon, nitrogen, water }));
 }
+
+export function getMetrics() {
+    const metrics = localStorage.getItem('metricsSession');
+    return metrics ? JSON.parse(metrics) : null;
+}
