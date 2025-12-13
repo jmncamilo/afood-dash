ca53d8a75c14f06c5656ab406c5134d9a2288265
jmncamilo <jmncamilo@gmail.com>
2025-12-13 01:55:26 -0500

    feat(client/login): add custom messages in an alert modal to improve user experience feedback

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -8,6 +8,9 @@ import { useRouter } from "next/navigation";
 import { setSession, getSession } from "@/lib/utils/authSession";
 import { useEffect, useState } from "react";
 import { Loader } from "@/components/common/Loader";
+import { AlertModal } from "@/components/modals/alert/AlertModal";
+import { useObjectState } from "@/hooks/useObjectState";
+
 
 export default function Login() {
     // Usa el hook de next para redireccionamiento
@@ -37,25 +40,51 @@ export default function Login() {
         handlerSetForm(e);
     };
 
+    // Custom hook para manejar la data del modal y controlar su visibilidad
+    const { objectData:modalData, updateStateByKey } = useObjectState({ isOpenModal: false, modalImage: '', modalTitle: '', modalMessage: '' });
+        // Handler para abrir el modal
+    const handleOpenModal = () => {
+        updateStateByKey('isOpenModal', true);
+    };
+        // Handler para setear información del modal
+    const handleSetDataModal = (image, title, message) => {
+        updateStateByKey('modalImage', image);
+        updateStateByKey('modalTitle', title);
+        updateStateByKey('modalMessage', message);
+    };
+
     // Handler con la lógica del login
     const handleLogin = () => {
         try {
             // Valida que el input no esté vacío
-            if (!form.customerNit) return alert('Debes ingresar el NIT de tu empresa...'); // TODO: modal
+            if (!form.customerNit) {
+                handleSetDataModal('/icons/3d-megaphone.png', '¡Mmm!', 'Por favor, ingresa el NIT de tu empresa para poder continuar con el inicio de sesión.');
+                handleOpenModal();
+                return;
+            }
             // Valida que el input tenga más de 5 caracteres
-            if (form.customerNit.length < 6) return alert('El NIT ingresado no cumple con el formato legal requerido. Por favor, verifica e intenta nuevamente.');
+            if (form.customerNit.length < 6) {
+                handleSetDataModal('/icons/3d-thumb-down.png', '¡Oops!', 'El NIT ingresado no cumple con el formato legal requerido. Por favor, verifica e intenta nuevamente.');
+                handleOpenModal();
+                return;
+            }
             // Valida que el NIT se encuentre en el diccionario
             const customerNameQuery = validateNit(form.customerNit);
-            if (!customerNameQuery) return alert('El NIT ingresado no está registrado como cliente de afood. Por favor, verifica e intenta nuevamente.'); // TODO: modal
+            if (!customerNameQuery) {
+                handleSetDataModal('/icons/3d-megaphone.png', '¡Lo sentimos!', 'El NIT ingresado no está registrado como cliente de afood. Por favor, verifica e intenta nuevamente.');
+                handleOpenModal();
+                return;
+            }
             // Si el código llega hasta acá, quiere decir que encontró al cliente
             setSession(form.customerNit, customerNameQuery);
             // Respuesta exitosa y redireccionamiento
-            alert('¡Login exitoso!');
+            alert('¡Login exitoso!'); // TESTING CJ
             router.push('/dashboard');
         } catch (error) {
             // Manejando errores inesperados
-            alert('Ocurrió un error inesperado. Por favor, intenta nuevamente.'); // TODO: modal
             console.error(error);
+            handleSetDataModal('/icons/3d-thumb-down.png', '¡Algo falló!', 'No pudimos completar tu solicitud en este momento. Por favor, verifica los datos ingresados y vuelve a intentarlo.');
+            handleOpenModal();
         }
     };
 
@@ -114,6 +143,16 @@ export default function Login() {
                     </div>
                 </main>
             </div>
+
+            {/* Modal para mensajes de alerta en el inicio de sesión */}
+            <AlertModal
+                isOpen={modalData.isOpenModal}
+                onClose={() => updateStateByKey('isOpenModal', false)}
+                srcImage={modalData.modalImage}
+                title={modalData.modalTitle}
+                message={modalData.modalMessage}
+            />
+
             {/* Componente loader */}
             {isLoading && <Loader/>}
         </div>
